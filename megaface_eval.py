668489bde7d999e9a73b87bbaf56cdf6936ac3d0
Yang Liu <liuyang12@focusmedia.cn>
2019-05-05 14:23:03 +0800

    update

--- megaface_eval.py
+++ megaface_eval.py
@@ -2,23 +2,23 @@ import subprocess
 
 if __name__ == '__main__':
     cmd = 'find megaface/facescrub_images -name "*.bin" -type f -delete'
-    output = subprocess.check_output(cmd, shell=True)
+    output = subprocess.check_output(cmd, shell=True).decode("utf-8")
     print(output)
 
     cmd = 'find megaface/MegaFace_aligned/FlickrFinal2 -name "*.bin" -type f -delete'
-    output = subprocess.check_output(cmd, shell=True)
+    output = subprocess.check_output(cmd, shell=True).decode("utf-8")
     print(output)
 
     cmd = 'python3 megaface.py --action gen_features'
-    output = subprocess.check_output(cmd, shell=True)
+    output = subprocess.check_output(cmd, shell=True).decode("utf-8")
     print(output)
 
     cmd = 'python3 megaface.py --action gen_features'
-    output = subprocess.check_output(cmd, shell=True)
+    output = subprocess.check_output(cmd, shell=True).decode("utf-8")
     print(output)
 
     cmd = 'python run_experiment.py -p /dev/code/mnt/InsightFace-v3/megaface/devkit/templatelists/facescrub_uncropped_features_list.json /dev/code/mnt/InsightFace-v3/megaface/MegaFace_aligned/FlickrFinal2 /dev/code/mnt/InsightFace-v3/megaface/facescrub_images _0.bin results -s 1000000'
-    output = subprocess.check_output(cmd, shell=True)
+    output = subprocess.check_output(cmd, shell=True).decode("utf-8")
     print(output)
 
     lines = output.split('\n')
