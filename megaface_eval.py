025ab8ceec0b56f83c33b2fd0dfb76b877936e7b
Yang Liu <liuyang12@focusmedia.cn>
2019-05-05 14:14:29 +0800

    update

--- megaface_eval.py
+++ megaface_eval.py
@@ -2,26 +2,27 @@ import subprocess
 
 if __name__ == '__main__':
     cmd = 'find megaface/facescrub_images -name "*.bin" -type f -delete'
-    result = subprocess.run([cmd], stdout=subprocess.PIPE)
-    print(result.stdout)
+    result = subprocess.check_output(cmd, shell=True)
+    print(result.communicate()[0])
 
     cmd = 'find megaface/MegaFace_aligned/FlickrFinal2 -name "*.bin" -type f -delete'
-    result = subprocess.run([cmd], stdout=subprocess.PIPE)
-    print(result.stdout)
+    result = subprocess.check_output(cmd, shell=True)
+    print(result.communicate()[0])
 
     cmd = 'python3 megaface.py --action gen_features'
-    result = subprocess.run([cmd], stdout=subprocess.PIPE)
-    print(result.stdout)
+    result = subprocess.check_output(cmd, shell=True)
+    print(result.communicate()[0])
 
     cmd = 'python3 megaface.py --action gen_features'
-    result = subprocess.run([cmd], stdout=subprocess.PIPE)
-    print(result.stdout)
+    result = subprocess.check_output(cmd, shell=True)
+    print(result.communicate()[0])
 
     cmd = 'python run_experiment.py -p /dev/code/mnt/InsightFace-v3/megaface/devkit/templatelists/facescrub_uncropped_features_list.json /dev/code/mnt/InsightFace-v3/megaface/MegaFace_aligned/FlickrFinal2 /dev/code/mnt/InsightFace-v3/megaface/facescrub_images _0.bin results -s 1000000'
-    result = subprocess.run([cmd], stdout=subprocess.PIPE)
-    print(result.stdout)
+    result = subprocess.check_output(cmd, shell=True)
+    output = result.communicate()[0]
+    print(output)
 
-    lines = result.stdout.split('\n')
-    line = [l for l in lines if l.startswith('Rank 1: ')]
+    lines = output.split('\n')
+    line = [l for l in lines if l.startswith('Rank 1: ')][0]
     accuracy = float(line[8:])
     print(accuracy)
