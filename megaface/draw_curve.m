f3fc9cde97182ce1e1256bfa13e39ac5b552ff0e
Yang Liu <liuyang12@focusmedia.cn>
2019-05-30 09:36:13 +0800

    update

--- megaface/draw_curve.m
+++ megaface/draw_curve.m
@@ -2,7 +2,7 @@
 
 format long
 addpath('D:\Users\foamliu\code\jsonlab');
-facescrub_cmc_file = 'D:\Users\foamliu\code\InsightFace-v2\megaface\results\cmc_facescrub_megaface_0_1000000_1.json'
+facescrub_cmc_file = 'D:\Users\foamliu\code\InsightFace-v3\megaface\results\cmc_facescrub_megaface_0_1000000_1.json'
 facescrub_cmc_json = loadjson(fileread(facescrub_cmc_file));
 facescrub_cmc_json
 
@@ -21,13 +21,13 @@ facescrub_cmc_json.roc(1,:)
 
 figure(2);
 %semilogx(facescrub_cmc_json.roc(1,:),facescrub_cmc_json.roc(2,:),'LineWidth',2);
-xdata=[0.0,			1.028475438147325e-08,			3.085426314441975e-08,			2.262646034978388e-07,			9.976212140827556e-07,			6.757083610864356e-06,			0.0001522452221252024,			1.0		],
-ydata=[0.9281878471374512,			0.9383026361465454,			0.9504063129425049,			0.9605637192726135,			0.9707068800926208,			0.9807222485542297,			0.9907233715057373,			1.0]
+xdata=[0.0,			6.170852628883949e-08,			1.851255859719458e-07,			7.816413472028216e-07,			4.607570190273691e-06,			5.529083864530548e-05,			1.0		],
+ydata=[0.9389561414718628,			0.9498948454856873,			0.9601233005523682,			0.9702096581459045,			0.9802108407020569,			0.9902119636535645,			1.0]
 semilogx(xdata,ydata,'LineWidth',2);
 %semilogx(facescrub_cmc_json.roc{1},facescrub_cmc_json.roc{2},'LineWidth',2);
 title(['Verification @ 1e-6 = ' num2str(interp1(xdata, ydata, 1e-6))]);
 xlim([1e-6 1]);
-%ylim([0 1]);
+ylim([0 1]);
 xlabel('False Positive Rate');
 ylabel('True Positive Rate');
 grid on;
