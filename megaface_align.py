90666d92d95b8ab833102b766aeb1e58b1d525a4
Yang Liu <liuyang12@focusmedia.cn>
2019-10-10 09:30:17 +0800

    udpate

--- megaface_align.py
+++ megaface_align.py
@@ -1,8 +1,20 @@
 import os
+from multiprocessing import Pool
+
+import tqdm
 from tqdm import tqdm
 
-def detect_face():
-    pass
+from utils import get_central_face_attributes, align_face
+
+
+def detect_face(image_path):
+    dirName = image_path['dirName']
+    fname = image_path['fname']
+    full_path = os.path.join(dirName, fname)
+    has_face, bboxes, landmarks = get_central_face_attributes(full_path)
+    if has_face:
+        img = align_face(full_path, landmarks)
+    return True
 
 
 def megaface_align(src, dst):
@@ -10,14 +22,16 @@ def megaface_align(src, dst):
     for dirName, subdirList, fileList in tqdm(os.walk(src)):
         for fname in fileList:
             if fname.lower().endswith('.jpg'):
-                image_paths.append({'dirName':dirName, 'fname':fname})
+                image_paths.append({'dirName': dirName, 'fname': fname})
 
     print(image_paths[:20])
-    print(len(image_paths))
+    num_images = len(image_paths)
+    print(num_images)
 
+    with Pool(16) as p:
+        r = list(tqdm.tqdm(p.imap(detect_face, image_paths), total=num_images))
 
-    # with Pool(2) as p:
-    #     r = list(tqdm.tqdm(p.imap(_foo, range(30)), total=30))
+    print(r)
 
 
 if __name__ == '__main__':
