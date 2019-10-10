479ffc5d60c72c06947be4194d409912ff547807
Yang Liu <liuyang12@focusmedia.cn>
2019-10-10 13:27:25 +0800

    update

--- megaface_align.py
+++ megaface_align.py
@@ -1,5 +1,4 @@
 import os
-from torch.multiprocessing import Pool
 
 import cv2 as cv
 import tqdm
@@ -32,8 +31,11 @@ def megaface_align(src, dst):
     num_images = len(image_paths)
     print(num_images)
 
-    with Pool(16) as p:
-        r = list(tqdm(p.imap(detect_face, image_paths), total=num_images))
+    # with Pool(16) as p:
+    #     r = list(tqdm(p.imap(detect_face, image_paths), total=num_images))
+
+    for image_path in tqdm(image_paths):
+        detect_face(image_path)
 
     print('Completed!')
 
