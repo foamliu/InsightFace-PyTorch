211653a7730ab20172691dff394237b4d69a7f08
Yang Liu <liuyang12@focusmedia.cn>
2019-10-10 09:42:03 +0800

    udpate

--- megaface_align.py
+++ megaface_align.py
@@ -1,19 +1,21 @@
 import os
 from multiprocessing import Pool
 
+import cv2 as cv
 import tqdm
 from tqdm import tqdm
 
 from utils import get_central_face_attributes, align_face
 
 
-def detect_face(image_path):
-    dirName = image_path['dirName']
-    fname = image_path['fname']
-    full_path = os.path.join(dirName, fname)
-    has_face, bboxes, landmarks = get_central_face_attributes(full_path)
+def detect_face(data):
+    src_path = data['src_path']
+    dst_path = data['dst_path']
+    has_face, bboxes, landmarks = get_central_face_attributes(src_path)
     if has_face:
-        img = align_face(full_path, landmarks)
+        img = align_face(src_path, landmarks)
+        cv.imwrite(dst_path, img)
+
     return True
 
 
@@ -22,7 +24,9 @@ def megaface_align(src, dst):
     for dirName, subdirList, fileList in tqdm(os.walk(src)):
         for fname in fileList:
             if fname.lower().endswith('.jpg'):
-                image_paths.append({'dirName': dirName, 'fname': fname})
+                src_path = os.path.join(dirName, fname)
+                dst_path = os.path.join(dirName.replace(src, dst), fname)
+                image_paths.append({'src_path': src_path, 'dst_path': dst_path})
 
     print(image_paths[:20])
     num_images = len(image_paths)
@@ -31,7 +35,7 @@ def megaface_align(src, dst):
     with Pool(16) as p:
         r = list(tqdm(p.imap(detect_face, image_paths), total=num_images))
 
-    print(r)
+    print('Completed!')
 
 
 if __name__ == '__main__':
