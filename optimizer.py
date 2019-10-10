625bb21c294eda61718a899662d8c9acdef5b96c
Yang Liu <liuyang12@focusmedia.cn>
2019-10-10 09:59:41 +0800

    udpate

--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,3 @@
-from utils import _clip_gradient, _adjust_learning_rate
-
-
 class InsightFaceOptimizer(object):
     """A simple wrapper class for learning rate scheduling"""
 
@@ -25,7 +22,10 @@ class InsightFaceOptimizer(object):
                 param_group['lr'] = self.lr
 
     def clip_gradient(self, grad_clip):
-        _clip_gradient(self.optimizer, grad_clip)
+        for group in self.optimizer.param_groups:
+            for param in group['params']:
+                if param.grad is not None:
+                    param.grad.data.clamp_(-grad_clip, grad_clip)
 
     def adjust_learning_rate(self, new_lr):
         for param_group in self.optimizer.param_groups:
