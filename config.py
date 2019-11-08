f8c6d19876ca489de72ac9b9ca6ba0942af15490
Yang Liu <liuyang12@focusmedia.cn>
2019-11-08 14:33:19 +0800

    update

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ channel = 3
 emb_size = 512
 
 # Training parameters
-num_workers = 1  # for data-loading; right now, only 1 works with h5py
+num_workers = 4  # for data-loading; right now, only 1 works with h5py
 grad_clip = 5.  # clip gradients at an absolute value of
 print_freq = 100  # print training/validation stats  every __ batches
 checkpoint = None  # path to checkpoint, None if none
