08b50474e7284b463287d54a646a62922198e6cd
Yang Liu <liuyang12@focusmedia.cn>
2019-12-02 09:03:53 +0800

    update

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ print_freq = 100  # print training/validation stats  every __ batches
 checkpoint = None  # path to checkpoint, None if none
 
 # Data parameters
-num_classes = 85164
-num_samples = 3804846
+num_classes = 85742
+num_samples = 5822653
 DATA_DIR = 'data'
 faces_ms1m_folder = 'data/faces_emore'
 path_imgidx = os.path.join(faces_ms1m_folder, 'train.idx')
--- pre_process.py
+++ pre_process.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     samples = []
     class_ids = set()
 
-    # %% 1 ~ 3804847
-    for i in tqdm(range(10000000)):
+    # %% 1 ~ 5822653
+    for i in tqdm(range(5822653)):
         try:
             header, s = recordio.unpack(imgrec.read_idx(i + 1))
             img = mx.image.imdecode(s).asnumpy()
