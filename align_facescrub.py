d11f755800aa6c290c99a5914f60f66a08a836a5
Yang Liu <liuyang12@focusmedia.cn>
2019-12-26 16:02:03 +0800

    update

--- align_facescrub.py
+++ align_facescrub.py
@@ -129,7 +129,7 @@ def align_facescrub(src, dst):
     num_images = len(image_paths)
     print('num_images: ' + str(num_images))
 
-    with Pool(4) as p:
+    with Pool(3) as p:
         r = list(tqdm(p.imap(detect_face, image_paths), total=num_images))
 
     # for image_path in image_paths:
