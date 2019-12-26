90dc44f9a2a3cd0111daa2a757ba79b8f5b4a4ed
Yang Liu <liuyang12@focusmedia.cn>
2019-12-26 21:13:39 +0800

    update

--- config.py
+++ config.py
@@ -18,10 +18,11 @@ print_freq = 100  # print training/validation stats  every __ batches
 checkpoint = None  # path to checkpoint, None if none
 
 # Data parameters
-num_classes = 85742
-num_samples = 5822653
+num_classes = 93431
+num_samples = 5179510
 DATA_DIR = 'data'
-faces_ms1m_folder = 'data/faces_emore'
+# faces_ms1m_folder = 'data/faces_ms1m_112x112'
+faces_ms1m_folder = 'data/ms1m-retinaface-t1'
 path_imgidx = os.path.join(faces_ms1m_folder, 'train.idx')
 path_imgrec = os.path.join(faces_ms1m_folder, 'train.rec')
 IMG_DIR = 'data/images'
