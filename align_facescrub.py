be991b885573ac09e90c7b707eade35049a5f208
Yang Liu <liuyang12@focusmedia.cn>
2019-12-26 15:31:41 +0800

    udate

--- align_facescrub.py
+++ align_facescrub.py
@@ -3,11 +3,6 @@ import os
 if __name__ == "__main__":
     annotation_files = ['facescrub_actors.txt', 'facescrub_actresses.txt']
 
-    files = []
-    for dirName, subdirList, fileList in os.walk('megaface/FaceScrub'):
-        for fname in fileList:
-            print(fname)
-
     samples = []
 
     for anno in annotation_files:
@@ -20,11 +15,13 @@ if __name__ == "__main__":
                 tokens = line.split('\t')
                 name = tokens[0]
                 face_id = tokens[2]
+                url = tokens[3]
+                ext = url.split('.')[-1]
                 bbox = tokens[4]
-                for ext in ['jpg', 'jpeg', 'png', 'gif']:
-                    filename = 'megaface/FaceScrub/{0}/{0}_{1}.{2}'.format(name, face_id, ext)
-                    if os.path.isfile(filename):
-                        samples.append({'name': name, 'face_id': face_id, 'bbox': bbox, 'ext': ext})
-                        break
+                filename = '{0}_{1}.{2}'.format(name, face_id, ext)
+                full_path = 'megaface/FaceScrub/{0}/{1}'.format(name, filename)
+                if os.path.isfile(filename):
+                    samples.append({'name': name, 'face_id': face_id, 'bbox': bbox, 'ext': ext})
+                    break
 
     print(len(samples))
