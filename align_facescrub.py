bf3508473f0b9da23d68d93c7564b9bd282cf080
Yang Liu <liuyang12@focusmedia.cn>
2019-12-26 14:13:13 +0800

    update

--- align_facescrub.py
+++ align_facescrub.py
@@ -21,6 +21,30 @@ def resize(img):
     return img
 
 
+def bb_intersection_over_union(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value
+    return iou
+
+
 def detect_face(data):
     from utils import select_significant_face, align_face
     from retinaface.detector import detector
@@ -49,6 +73,9 @@ def detect_face(data):
 
 
 def align_megaface(src, dst, size):
+
+
+
     image_paths = []
     for dirName, subdirList, fileList in tqdm(os.walk(src)):
         for fname in fileList:
@@ -57,24 +84,24 @@ def align_megaface(src, dst, size):
                 dst_path = os.path.join(dirName.replace(src, dst), fname).replace(' ', '_')
                 image_paths.append({'src_path': src_path, 'dst_path': dst_path})
 
-    # print(image_paths[:20])
-    num_images = len(image_paths)
-    print('num_images: ' + str(num_images))
-
-    with Pool(size) as p:
-        r = list(tqdm(p.imap(detect_face, image_paths), total=num_images))
-
-    # for image_path in tqdm(image_paths):
-    #     detect_face(detector, image_path)
-
-    print('Completed!')
+    # # print(image_paths[:20])
+    # num_images = len(image_paths)
+    # print('num_images: ' + str(num_images))
+    #
+    # with Pool(size) as p:
+    #     r = list(tqdm(p.imap(detect_face, image_paths), total=num_images))
+    #
+    # # for image_path in tqdm(image_paths):
+    # #     detect_face(detector, image_path)
+    #
+    # print('Completed!')
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train face network')
+    parser = argparse.ArgumentParser(description='align FaceScrub')
     # general
-    parser.add_argument('--src', type=str, default='megaface/MegaFace', help='src path')
-    parser.add_argument('--dst', type=str, default='megaface/MegaFace_aligned', help='dst path')
+    parser.add_argument('--src', type=str, default='megaface/FaceScrub', help='src path')
+    parser.add_argument('--dst', type=str, default='megaface/FaceScrub_aligned', help='dst path')
     parser.add_argument('--size', type=int, default=4, help='processes')
 
     args = parser.parse_args()
@@ -90,4 +117,4 @@ if __name__ == '__main__':
 
     align_megaface(src, dst, size)
 
-    # python3 align_megaface.py --src megaface/MegaFace --dst megaface/MegaFace_aligned
+    # python3 align_facescrub.py --src megaface/FaceScrub --dst megaface/FaceScrub_aligned
