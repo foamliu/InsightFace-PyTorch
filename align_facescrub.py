c63fbac13b05a5812c2c55a90867161c75c430f3
Yang Liu <liuyang12@focusmedia.cn>
2019-12-26 16:01:04 +0800

    update

--- align_facescrub.py
+++ align_facescrub.py
@@ -1,7 +1,9 @@
 import argparse
 import os
+from multiprocessing import Pool
 
 import cv2 as cv
+from tqdm import tqdm
 
 
 def resize(img):
@@ -127,12 +129,12 @@ def align_facescrub(src, dst):
     num_images = len(image_paths)
     print('num_images: ' + str(num_images))
 
-    # with Pool(size) as p:
-    #     r = list(tqdm(p.imap(detect_face, image_paths), total=num_images))
+    with Pool(4) as p:
+        r = list(tqdm(p.imap(detect_face, image_paths), total=num_images))
 
-    for image_path in image_paths:
-        detect_face(image_path)
-        break
+    # for image_path in image_paths:
+    #     detect_face(image_path)
+    #     break
 
     print('Completed!')
 
